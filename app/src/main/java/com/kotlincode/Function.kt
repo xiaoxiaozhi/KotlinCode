ca1a4712a6cf167aa50f843ed9c268325eddde95
zhixun <327354573@qq.com>
2021-09-16 17:17:01 +0800

    函数

---
+++ app/src/main/java/com/kotlincode/Function.kt
@@ -0,0 +1,62 @@
+package com.kotlincode
+
+/*
+ *函数，
+ * 1.所有的函数都可以当做表达式
+ * 2.根据1所以函数都有返回值，如果没有明确指定则返回Unit
+ * 3.参数必须指定类型,参数都被val修饰不能，函数体内参数不能被修改
+ * 4.默认参数一般放在参数列表之后
+ */
+fun main() {
+    println(greet())
+    println(greet("Eva"))
+    println(notReally())
+    println(notReally(1))
+//    switch("ok")
+    createPerson("jack", height = 190, weight = 90)//对所有其它参数使用命名参数的时候，可以忽略age
+    println("最大值:${max(1, 8, 3, 4)}")
+    greetMany("hello", "tom", "jerry")
+}
+
+//1. 创建一个最小函数: fun 函数名 参数列表(可以为空) ，如果函数体只是一个表达式，
+// 那么使用=运算符而不是{},这样的函数叫做短函数，只有函数是表达式而不是{}的时候kotlin才会进行类型推断，所以return不能用于短函数
+fun greet() = "hello"
+
+//2.Unit  如果表达式没有返回任何内容，kotlin奖把类型推断为Unit
+fun sayHello() = println("hello")
+
+//3.参数 必须指定类型，所有的参数都是val修饰，在函数体不能被修改
+fun greet(name: String): String {
+//    name = name.toUpperCase();
+    return "hello $name"
+}
+
+//4.带有{}块的函数
+//fun notReally() :Int= { 2 } 返回类型 = {} 这种形式是错误的
+fun notReally() = { 2 }// kotlin 会认为=后面跟{} 是一个lambda表达式、或者是一个匿名函数
+fun notReally(value: Int) = { n: Int -> n * value }// kotlin 推断也是一个lambda表达式，={}做法kotlin极不推荐
+
+//5. 默认参数(变量名:类型 = 默认值) 在本例中默认参数还可以是一个字符串模板 msg:String ="${name.length}"
+fun greet(name: String, msg: String = "hello") = "$msg,$name"
+
+//5.1默认参数通常放在参数列表最后，调换也是可以的,但是这样做，会被迫给默认参数一个值，违背了默认参数的目的
+fun switch(name: String = "Eva", msg: String) = "$msg,$name"
+
+//6.命名参数,使用命名参数提高可读性, createPerson("jack",18,190,90) 调用时这样的一段代码可能看不懂，这时候使用命名参数提高可读性
+fun createPerson(name: String, age: Int = 18, height: Int = 189, weight: Int = 80) =
+    println("$name,$age $height $weight")
+
+//7.可变数量参数,  numbers 类型被指定为 Int 而不是IntArray， 关键字vararg 将参数注释为指定类型的数组
+//如果函数有多个参数的时候，只有一个参数能被vararg修饰
+fun max(vararg numbers: Int): Int {
+    var large = Int.MIN_VALUE
+    for (number: Int in numbers) {
+        large = if (number > large) number else large
+    }
+    return large
+}
+
+//7.1 函数接收两个参数其中易格斯可变数量参数, 可变参数最好在最后一个，否则 其它参数就要使用命名参数
+fun greetMany(msg: String, vararg names: String) = println("$msg  ${names.joinToString("、")}")
+
+
--- app/src/main/java/com/kotlincode/KotlinBase.kt
+++ app/src/main/java/com/kotlincode/KotlinBase.kt
@@ -1,7 +1,58 @@
 package com.kotlincode
 
+import java.lang.Exception
+import java.util.*
+
+/**
+ * 语句与表达式
+ */
 fun main() {
     val greet = "hello"
     println(greet::class)
     println(greet.javaClass)
+    var factor = 2;
+    fun doubleInt(arg: Int): Int = arg * factor;
+    factor = 1;
+    print(doubleInt(2))
+    //1. 字符串
+    println("$factor")//①字符串模板--->${变量、表达式、函数}也可以省略{} 例如 $变量
+    println("转义字符串\$,\"are you ok\"")//②转义字符串使用的越多，越混乱；
+    println("""\n fox say"are you ok"  """);//③原始字符串三个" 内部也可以使用字符串模板${}，但是没有转义字符
+    println(
+        """
+    Tell me and I forget.
+    |Teach me and I remember.
+    |Involve me and I learn.
+    |(Benjamin Franklin)
+    """.trimMargin()
+    )//④多行字符串通原始字符串一样以三个"开始，三个"结束。除了"""所在的第一行，剩余每行默认有一个缩进，trimMargin()函数消除缩进直到|
+    // 如果不想用|做分隔符也可以使用其他字符 trimMargin("<")
+    //2. 更多的表达式更少的语句
+    val introduce = if (Date().time > 0) "我今年18岁" else "未满18岁"//①if是一个表达式 ，根据年龄获取描述，想想java在这里会怎么做
+    println("$introduce")
+    println(tryExpr(true))//②try catch 也是一个表达式，没有异常的情况下 try 代码块最后一条表达式将成为结果，有异常的情况下则是catch
+    //3.赋值不是表达式
+    var a = 1
+    var b = 2
+    var c = 3
+    a = b      //赋值
+    println(a)
+    //a = b = c//这种情况会报错 赋值不是表达式 相当于 a= (b=c) 由于b=c不是表达式 ，所以不能给a赋值
 }
+
+fun tryExpr(bloweUp: Boolean): Int {
+    return try {
+        if (bloweUp) {
+            throw Exception();
+        } else {
+            3
+        }
+    } catch (e: Exception) {
+        4
+    } finally {
+        5
+    }
+
+}
+
+
