f5464bb95c2522806196bdabb9b2332132331960
zhixun <327354573@qq.com>
2021-09-18 18:00:46 +0800

    类型安全

---
+++ app/src/main/java/com/kotlincode/TypeSafety.kt
@@ -0,0 +1,14 @@
+package com.kotlincode
+
+/**
+ * 类型安全
+ */
+fun main() {
+    println("-----Any-----")
+    anyClass()
+}
+
+//1. any  kotlin中所有的类都继承自Any，目的是为了提供一些通用的方法 equals、hashCode、toString
+fun anyClass() {
+
+}
