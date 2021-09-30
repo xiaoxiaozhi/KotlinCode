a1a7c663839c0380b67fb87981e9116c913a7f46
zhixun <327354573@qq.com>
2021-09-30 18:13:55 +0800

    fanxing4

--- app/src/main/java/com/kotlincode/typesafe/Generic.kt
+++ app/src/main/java/com/kotlincode/typesafe/Generic.kt
@@ -4,9 +4,8 @@ package com.kotlincode.typesafe
  * 泛型
  */
 fun main() {
-    //1. 类型不变性(Array<T>和List<out E>)
-    val bananas: Array<Banana> = arrayOf<Banana>()
-    arrayOf<Fruit>() + Orange()
+    //1. 函数参数类型不变性(Array<T>和List<out E>)
+    val bananas = arrayOf<Banana>()
 //    receiveFruit(bananas)// 同java一样不允许传递，泛型类型不变性决定，理由：一筐子香蕉不是从一筐子水果继承来的
     val orange: List<Orange> = listOf();
     receiveFruit(orange)//限制了数组，但没有限制列表,原因是数组接口和列表接口后者有向下兼容List<out E>
